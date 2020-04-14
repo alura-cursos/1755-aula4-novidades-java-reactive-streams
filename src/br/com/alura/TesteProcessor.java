b6dff8f04efe663579be987e1fbe6fbcf63bbc06
João Victor <jvc.martins12@gmail.com>
2020-04-14 18:20:53 -0300

    Alterações iniciais

--- src/br/com/alura/TesteProcessor.java
+++ src/br/com/alura/TesteProcessor.java
@@ -5,27 +5,24 @@ import java.util.Scanner;
 import java.util.concurrent.SubmissionPublisher;
 
 import br.com.alura.model.NotaFiscal;
-import br.com.alura.subscriber.NotaFiscalSubscriber;
+import br.com.alura.processor.Processor;
 
 public class TesteProcessor {
 
 	public static void main(String[] args) {
-
-		NotaFiscalSubscriber nfs = new NotaFiscalSubscriber();
+		System.out.println(Thread.currentThread().getName());
 		SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<NotaFiscal>();
 
-		publisher.subscribe(nfs);
+		publisher.consume(Processor::emitir);
 
 		NotaFiscal notaFiscal = new NotaFiscal("João", 39.99, LocalDate.now());
-		NotaFiscal notaFiscalMag = new NotaFiscal("Mag", 40.00, LocalDate.now());
 
 		publisher.submit(notaFiscal);
-		publisher.submit(notaFiscalMag);
-		publisher.close();
 
 		System.out.println("Parabéns pela compra!!");
 		Scanner scanner = new Scanner(System.in);
 		scanner.nextLine();
 		scanner.close();
+		publisher.close();
 	}
 }
