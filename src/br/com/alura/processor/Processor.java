836c901b6beef583da0efe483594720c751b7941
João Victor <jvc.martins12@gmail.com>
2020-04-14 18:07:35 -0300

    Segunda aula usando fluxos reativos

--- src/br/com/alura/processor/Processor.java
+++ src/br/com/alura/processor/Processor.java
@@ -6,12 +6,9 @@ public class Processor {
 
 	public static void emitir(NotaFiscal nf) {
 		try {
-			String name = Thread.currentThread().getName();
 			System.out.println("Emitindo nota fiscal");
 			Thread.sleep(5000);
-			System.out.format("Nota fiscal emitida %nEm nome de %s %nNa data de %s %n",
-					nf.getNome(), nf.getData());
-			System.out.println("A thread do processor é: " + name);
+			System.out.format("Nota fiscal emitida %nEm nome de %s %nNa data de %s %n", nf.getNome(), nf.getData());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
