5011a84f5fd99e01652875941c89eda921f8c197
João Victor <jvc.martins12@gmail.com>
2020-04-16 18:06:06 -0300

    Renomear classes e pacotes

---
+++ src/br/com/alura/TesteEnvioNaoBloqueante.java
@@ -0,0 +1,34 @@
+package br.com.alura;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.SubmissionPublisher;
+
+import br.com.alura.model.NotaFiscal;
+import br.com.alura.wsclient.NotaFiscalWSClient;
+
+public class TesteEnvioNaoBloqueante {
+
+	public static void main(String[] args) {
+		ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(2);
+		NotaFiscal primeiraNotaFiscal = new NotaFiscal("João", 39.99, LocalDate.now());
+		SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<>(newFixedThreadPool, 1);
+		NotaFiscalWSClient nfwsc = new NotaFiscalWSClient();
+		publisher.consume(nfwsc::enviar);
+		publisher.consume(data -> {
+			System.out.println("Outra thread: " + Thread.currentThread().getName());
+		});
+		publisher.consume(data -> {
+			System.out.println("Mais uma thread: " + Thread.currentThread().getName());
+		});
+		publisher.submit(primeiraNotaFiscal);
+		System.out.println("Você irá receber a nota fiscal no seu e-mail");
+		Scanner scan = new Scanner(System.in);
+		scan.nextLine();
+		scan.close();
+		publisher.close();
+	}
+
+}
