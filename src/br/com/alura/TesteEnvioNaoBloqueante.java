e3aea4d052eef541fa69af1edc35222db7f0fcda
João Victor <jvc.martins12@gmail.com>
2020-04-17 16:07:23 -0300

    Ajuste para implementação do subscriber

--- src/br/com/alura/TesteEnvioNaoBloqueante.java
+++ src/br/com/alura/TesteEnvioNaoBloqueante.java
@@ -2,32 +2,30 @@ package br.com.alura;
 
 import java.time.LocalDate;
 import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.SubmissionPublisher;
 
 import br.com.alura.model.NotaFiscal;
-import br.com.alura.wsclient.NotaFiscalWSClient;
+import br.com.alura.subscriber.NotaFiscalSubscriber;
 
 public class TesteEnvioNaoBloqueante {
 
 	public static void main(String[] args) {
-		ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(2);
 		NotaFiscal primeiraNotaFiscal = new NotaFiscal("João", 39.99, LocalDate.now());
-		SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<>(newFixedThreadPool, 1);
-		NotaFiscalWSClient nfwsc = new NotaFiscalWSClient();
-		publisher.consume(nfwsc::enviar);
-		publisher.consume(data -> {
-			System.out.println("Outra thread: " + Thread.currentThread().getName());
-		});
-		publisher.consume(data -> {
-			System.out.println("Mais uma thread: " + Thread.currentThread().getName());
-		});
+		NotaFiscal segundaNotaFiscal = new NotaFiscal("Renata", 39.99, LocalDate.now());
+		NotaFiscal terceiraNotaFiscal = new NotaFiscal("Paulo", 39.99, LocalDate.now());
+		SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<>();
+		NotaFiscalSubscriber subscriber = new NotaFiscalSubscriber();
+		
+		publisher.subscribe(subscriber);
 		publisher.submit(primeiraNotaFiscal);
+		publisher.submit(segundaNotaFiscal);
+		publisher.submit(terceiraNotaFiscal);
 		System.out.println("Você irá receber a nota fiscal no seu e-mail");
+		
 		Scanner scan = new Scanner(System.in);
 		scan.nextLine();
 		scan.close();
+		
 		publisher.close();
 	}
 
